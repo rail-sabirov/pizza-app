1e253a6e6f7c148a7c80dfc31b77d35efc042b40
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-11-17 11:28:39 -0500

    Routes with createBrowserRouter and RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { MouseEvent, useRef, useState } from 'react'
 import Button from './components/Button/Button'
 import Input from './components/Input/Input';
-import { Routes, Route } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
-import { Error } from './pages/Error/Error';
 
 function App() {
   // Типизация значения хука
@@ -29,11 +25,6 @@ function App() {
         <a href="/">Menu</a> | <a href="/cart">Cart</a>
       </div>
 
-      <Routes>
-        <Route path='/' element={ <Menu /> } />
-        <Route path='/cart' element={ <Cart /> } />
-        <Route path='*' element={ <Error /> } />
-      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+
+// Описываем роуты к страницам, ниже добавляем RouterProvider
+const router = createBrowserRouter([
+  { path: '/', element: <Menu /> },
+  { path: '/cart', element: <Cart /> },
+  { path: '*', element: <Error /> },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+      <RouterProvider router={router} />
+  </React.StrictMode>
 )
