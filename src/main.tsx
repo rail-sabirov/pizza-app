21d4310a7d59b55dd4c768c7de5dc01f59fe7c61
Rail Sabirov <rail.sabirov@gmail.com>
2023-11-25 11:02:30 -0500

    Create LAZY load Menu page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
 import { Layout } from './layouts/Layout/Layout.tsx';
@@ -11,20 +10,23 @@ import Product from './pages/Product/Product.tsx';
 import { PREFIX } from './helpers/API.ts';
 import axios from 'axios';
 
+// Для ленивой загрузки страницы с меню, импортируем только при переходе на нее
+const LazyMenuPage = lazy(() => import('./pages/Menu/Menu'));
+
 // Описываем роуты к страницам, ниже добавляем RouterProvider
 const router = createBrowserRouter([
-  { 
-    path: '/', 
-    element: <Layout />, 
+  {
+    path: "/",
+    element: <Layout />,
 
     // Вложенные роуты, будут отображаться в <Outlet />
     children: [
-      { path: '/', element: <Menu /> },
-      { path: '/cart', element: <Cart /> },
+      { path: "/", element: <LazyMenuPage /> },
+      { path: "/cart", element: <Cart /> },
 
       // Роут с параметров id
-      { 
-        path: '/product/:id', 
+      {
+        path: "/product/:id",
         element: <Product />,
 
         // ассинхронно вызываем метод, потому что там axios
@@ -35,14 +37,13 @@ const router = createBrowserRouter([
           return data;
         },
 
-        errorElement: <>Error, we have a problem</>
-      }
-    ]
+        errorElement: <>Error, we have a problem</>,
+      },
+    ],
   },
 
   // Обработка других, не зарегистрированных путей
-  { path: '*', element: <Error /> },
-  
+  { path: "*", element: <Error /> },
 ]);
 
 
